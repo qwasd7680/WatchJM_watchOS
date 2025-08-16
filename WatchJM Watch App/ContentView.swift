379af589ec35d4e7c293d042d9fc3bd64dba372d
Maverick Charmer <hifex7680@gmail.com>
2025-08-16 13:07:33 +0800

    Initial Commit

---
+++ WatchJM Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchJM Watch App
+//
+//  Created by 周敬博 on 2025/8/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchJM Watch App/WatchJMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchJMApp.swift
+//  WatchJM Watch App
+//
+//  Created by 周敬博 on 2025/8/16.
+//
+
+import SwiftUI
+
+@main
+struct WatchJM_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
