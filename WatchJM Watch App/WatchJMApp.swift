ee0628ca2871eb105a43d0c26933f99f30bc77ab
Maverick Charmer <hifex7680@gmail.com>
2025-08-20 23:23:26 +0800

    延长超时时间至60s，

--- WatchJM Watch App/WatchJMApp.swift
+++ WatchJM Watch App/WatchJMApp.swift
@@ -7,14 +7,16 @@
 
 import SwiftUI
 import SDWebImageWebPCoder
+import SDWebImageSwiftUI
 
 
 @main
 struct WatchJM_Watch_AppApp: App {
     init() {
-            let WebPCoder = SDImageWebPCoder.shared
-            SDImageCodersManager.shared.addCoder(WebPCoder)
-        }
+        let WebPCoder = SDImageWebPCoder.shared
+        SDImageCodersManager.shared.addCoder(WebPCoder)
+        SDWebImageDownloader.shared.config.downloadTimeout = 60
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
