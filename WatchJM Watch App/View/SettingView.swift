f45c7de841dcd23d8e2bc3b25306e1c6ff1c26ae
Maverick Charmer <hifex7680@gmail.com>
2025-09-21 20:01:38 +0800

    添加了修改API的功能，将延迟移动到设置页面

--- WatchJM Watch App/View/SettingView.swift
+++ WatchJM Watch App/View/SettingView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
-//TODO
 struct SettingView: View {
+    let NetWorkManager = Net()
+    @State var ms:String = "检查中..."
+    @AppStorage("jmurl") var jmurl: String = "https://qwasd12w-jmcomic-api.hf.space/v1"
+
     var body: some View {
-		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+                Form {
+                    Section(header: Text("API URL")) {
+                        TextField("Enter JMComic API URL", text: $jmurl)
+						HStack{
+							Text("连接延迟: \(ms)")
+							Spacer()
+							Text(ms.hasSuffix("ms") ? "✅" : "❌")
+						}
+                    }
+                }
+                .navigationTitle("Settings")
+        }
+        .onAppear{
+            Task{
+                await checkLatency()
+            }
+        }
+        .onChange(of: jmurl) {
+            Task {
+                await checkLatency()
+            }
+        }
+    }
+    
+    private func checkLatency() async {
+        self.ms = "检查中..."
+        do {
+            let currentDate = Date()
+            let timeInterval = currentDate.timeIntervalSince1970 * 1000
+            let latency = try await NetWorkManager.Check(jmurl: jmurl, timeInterval: timeInterval)
+            self.ms = "\(latency)ms"
+        } catch {
+            self.ms = "无法连接"
+            print("Latency Check Error: \(error)")
+        }
     }
 }
 
